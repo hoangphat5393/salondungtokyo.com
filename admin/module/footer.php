3149964328182567e3ecadc0c97a3a3d93e7ab84
Vũ Hoàng Phát <hoangphat5393@gmail.com>
2025-08-08 20:17:47 +0700

    update

--- admin/module/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="page-footer">
-	<div class="row">
-		<div class="col-xs-12 col-sm-6">
-			<span class="txt-color-white">GetAtz <span class="hidden-xs"> - Admin</span> © 2020</span>
-		</div>
-	</div>
-</div>
--- admin/module/nav_c.php
+++
@@ -1,9 +0,0 @@
-<?php ob_start(); require_once('../lib/atz.php');?>
-<?php 
-class Module_nav_controller extends atz{
-
-    public function __construct() {
-        parent::__construct();
-    }
-}
-?>
